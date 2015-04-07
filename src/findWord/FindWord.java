a1bac9131e821d875313c42f1df660bf5225ed55
SeungEun-Baek <sebaek@gmail.com>
2015-04-07 13:07:04 +0900

    print line number

--- src/findWord/FindWord.java
+++ src/findWord/FindWord.java
@@ -4,29 +4,29 @@ import java.io.File;
 import java.util.Scanner;
 
 public class FindWord {
-	
+
 	public static void main(String[] args) throws Exception {
-		
+
 		if (args.length == 0) {
 			System.out.println("Type path.");
 			return;
 		}
-		
-		String word = "tomcat";
-		
+
+		String word = "import";
+
 		File file = new File(args[0]);
-		
+
 		if (file.isFile()) {
 			findWord(file, word);
 		} else if (file.isDirectory()) {
 			scanDirectory(file, word);
 		}
-		
+
 	}
-	
+
 	private static void scanDirectory(File file, String word) throws Exception {
 		File[] files = file.listFiles();
-		
+
 		for (File file2 : files) {
 			if (file2.isFile()) {
 				findWord(file2, word);
@@ -34,23 +34,26 @@ public class FindWord {
 				scanDirectory(file2, word);
 			}
 		}
-		
+
 	}
-	
+
 	private static void findWord(File file, String word) throws Exception {
 		Scanner scanner = new Scanner(file);
-		String line;
-		
+		String lineStr;
+		int lineNum = 0;
+
 		while (scanner.hasNextLine()) {
-			line = scanner.nextLine();
-			if (line.contains(word)) {
-				System.out.println("Found! at " + file.getName());
+			lineNum++;
+			lineStr = scanner.nextLine();
+			if (lineStr.contains(word)) {
+				System.out.println("Found! at [" + file.getAbsolutePath() + ":"
+						+ lineNum + "]");
 			}
-			
+
 		}
-		
+
 		scanner.close();
-		
+
 	}
 
 }
